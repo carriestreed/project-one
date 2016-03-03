c49b37846dcb368cc96517df3abfaf4dfa01f572
Carrie Streed <cmstreed@gmail.com>
2016-03-03 13:23:42 -0500

    finally got both markers to move--the mistake was litterally a series of periods in the wrong spots

--- app-canvas-practice.js
+++ app-canvas-practice.js
@@ -15,6 +15,7 @@ var canvas;
 var ctx;
 var dx = 5;             //horizontal speed
 var dy = 5;             //vertical speed
+var circRadius  = 10;   //radius of circle
 var p1positionX = 10;   //p1 horizontal position
 var p1positionY = 305;  //p1 vertical position
 var p2positionX = 590;  //p2 horizontal position
@@ -36,13 +37,22 @@ ctx.fill();
 }
 
 
-function rect(p1positionX,p1positionY, w, h) {  //maze canvas
+function rect(x, y, w, h) {  //maze canvas for p1
 ctx.beginPath();
-ctx.rect(p1positionX, p1positionY, w, h);
+ctx.rect(x, y, w, h);
 ctx.closePath();
 ctx.fill();
 ctx.stroke();
 }
+
+
+// function rect(p2positionX,p2positionY, w, h) {  //maze canvas for p2
+// ctx.beginPath();
+// ctx.rect(p2positionX, p2positionY, w, h);
+// ctx.closePath();
+// ctx.fill();
+// ctx.stroke();
+// }
 
 function clear() { //allows to clear canvas (for movement)
 ctx.clearRect(0, 0, WIDTH, HEIGHT);
@@ -91,19 +101,19 @@ ARROW RIGHT = 39,
 function moveP2(p2){
   if (p2.keyCode === 39){
     console.log('moving p2 right');
-    p2.positionX += dx;
+    p2positionX += dx;
   }
   if (p2.keyCode === 37){
     console.log('moving p2 left');
-    p2.positionX -= dx;
+    p2positionX -= dx;
   }
   if (p2.keyCode === 38){
     console.log('moving p2 up');
-    p2.positionY -= dy;
+    p2positionY -= dy;
   }
   if (p2.keyCode === 40){
     console.log('moving p2 down');
-    p2.positionY += dy;
+    p2positionY += dy;
   }
 }
 
@@ -115,154 +125,12 @@ function draw() {
   ctx.strokeStyle = 'black';
   rect(0,0,WIDTH,HEIGHT);                 //rect is maze rectangle container
   ctx.fillStyle = 'MediumVioletRed';      //player 1 marker color
-  player1(p1positionX, p1positionY, 10);  //player 1 marker position
+  player1(p1positionX, p1positionY, circRadius);  //player 1 marker position
   ctx.fillStyle = 'YellowGreen';          //player 2 marker color
-  player2(p2positionX, p2positionY, 10);  //player 2 marker position
+  player2(p2positionX, p2positionY, circRadius);  //player 2 marker position
 }
 
 init();
 
 window.addEventListener('keydown',moveP1,true); //listens for p1 keydown
 window.addEventListener('keydown',moveP2,true); //listens for p2 keydown
-
-
-
-
-
-// //create player1
-// function player1() {
-//   ctx.fillStyle = 'deepPink';
-//   ctx.arc(p1positionX, p1positionY, 10, 0, Math.PI*2, true)
-// }
-//
-// function p1marker(x,y,w,h) {
-//   ctx.beginPath();
-//   ctx.arc(p1positionX, p1positionY, 10, 0, Math.PI*2, true);
-//   ctx.closePath();
-//   ctx.fill();
-// }
-
-
-
-// //create player2  (does not appear if function is positioned down????)
-// var player2 = function(){
-//   console.log('creating player2');
-//   ctx.fillStyle = 'yellowGreen';
-//   ctx.beginPath();
-//   ctx.arc(p2positionX, p2positionY, 10, 0, Math.PI*2, true);
-//   ctx.closePath();
-//   ctx.fill();
-// }
-// player2();
-
-
-
-/// PLAYER 1: D (RIGHT) A (LEFT) W (UP) S (DOWN)
-/* from Ascii table: http://www.ascii-code.com/
-D = 68, (right)
-A = 65, (left)
-W = 87, (up)
-S = 83  (down)
-*/
-//
-// window.addEventListener('keydown', movePlayer1, true);
-//
-// function movePlayer1(p1){
-//   if (p1.keyCode === 68){
-//     console.log('moving p1 right');
-//     clearCanvas();
-//     p1positionX +=5;
-//   }
-//   if (p1.keyCode === 65){
-//     console.log('moving p1 left');
-//   }
-//   if (p1.keyCode === 87){
-//     console.log('moving p1 up');
-//   }
-//   if (p1.keyCode === 83){
-//     console.log('moving p1 down');
-//   }
-// }
-//
-// function clearCanvas() {
-//   canvas.style.backgroundPositionX = p1positionX;
-// }
-
-
-
-//check for boundaries
-// function checkCanMove(){
-//   console.log('checking if player can move');
-// }
-
-
-
-
-
-
-
-
-
-
-////////////// PREVIOUS CODE (before canvas) ////////////////////
-
-//////// WELCOME WINDOW BOX
-/// GIVE SHORT GAME INSTRUCTIONS
-/// PROMPT USERS TO SELECT MARKER
-/// PLAYERS PRESS START
-/// WINDOW BOX CLOSES
-/// PLAYERS BEGIN GAME
-
-
-//////// SET GAME MARKERS' FUNCTIONALITY
-/// KEYSTROKES DETERMINE MOVEMENT OF MARKERS
-
-
-
-/*
-document.onkeydown = movePlayer1;
-
-function movePlayer1(marker){
-    if (marker.keyCode === 68){ //moves marker Right
-      checkCanMove();
-      player1start +=5;
-      player1.style.left = player1start + 'px';
-    }
-    if (marker.keyCode === 65){ //moves marker Left
-      player1start -=5;
-      player1.style.left = player1start + 'px';
-    }
-    if (marker.keyCode === 87) { //moves marker Up
-      player1start -=5;
-      player1.style.top = player1start + 'px';
-    }
-    if (marker.keyCode === 83) { //moves marker Down
-      player1start +=5;
-      player1.style.top = player1start + 'px';
-    }
-}
-
-// todo: set player initit position in javascript, not css
-
-
-
-
-/// PLAYER 2: ARROW LEFT, RIGHT, UP, DOWN
-/* from Ascii table: http://www.ascii-code.com/
-ARROW LEFT  = 37,
-      RIGHT = 39,
-      UP    = 38,
-      DOWN  = 40
-*/
-
-
-
-/////// SET WALL BOUNDARIES
-/// MARKERS CAN NOT PASS THROUGH BLACK WALLS
-
-
-/////// SET WIN
-/// FIRST PLAYER TO REACH OPPOSITE END WINS
-/// (SET PLAYER 1 WIN LOCATION / PLAYER 2 WIN LOCATION)
-/// WINDOW BOX ALERTS WHO IS THE WINNER
-/// PROMPT TO PLAY AGAIN
